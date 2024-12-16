434c361f7577a24afa678641ecc3e82dbc112999
Isabella Liu <isabellaliu0617@gmail.com>
2024-12-16 13:00:25 -0500

    updated source address

--- scripts/isaps-pats-for-nsrr.R
+++ scripts/isaps-pats-for-nsrr.R
@@ -7,11 +7,11 @@ library(tidyr)
 library(lubridate)
 
 
-setwd("/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_releases")
-
+data <- read.csv("/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_source/age_gender_interns 20240130.csv")
+data$timepoint <-1
+write.csv(data,file = "/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_releases/0.1.0.pre/isaps-dataset-0.1.0.pre.csv", row.names = FALSE, na='')
 
 # Harmonized data
-data <- read.csv("/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_releases/0.1.0/isaps-dataset-0.1.0.csv")
 harmonized_data<-data[,c("subject","age", "gender","timepoint")]%>%
   dplyr::mutate(nsrrid=subject,
                 nsrr_age=age,
@@ -21,4 +21,4 @@ harmonized_data<-data[,c("subject","age", "gender","timepoint")]%>%
                   TRUE ~ "not reported"
                 )) %>% select(nsrrid,timepoint,nsrr_age,nsrr_sex)
 
-write.csv(harmonized_data, file = "/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_releases/0.1.0/isaps-harmonized-dataset-0.1.0.csv", row.names = FALSE, na='')
+write.csv(harmonized_data, file = "/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_releases/0.1.0.pre/isaps-harmonized-dataset-0.1.0.pre.csv", row.names = FALSE, na='')
