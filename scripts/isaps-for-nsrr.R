f8e3c2f52d6bcc76916177d4767052a14493dc85
Isabella Liu <isabellaliu0617@gmail.com>
2025-01-10 12:52:59 -0500

    added file and folder names to dataset

--- scripts/isaps-for-nsrr.R
+++ scripts/isaps-for-nsrr.R
@@ -9,6 +9,23 @@ library(lubridate)
 
 data <- read.csv("/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_source/age_gender_interns 20240130.csv")
 data$timepoint <-1
+
+edf <- read.csv("/VOLUMES/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/edf-header-info/barger-filenames-processed.csv")
+id <- unique(substr(edf$ID, 1, 5))
+data$raw_folder_name <- NA
+data$filename_id <- NA
+
+for (i in 1:nrow(data)) {
+  subject <- data$subject[i]
+  match_id <- id[grep(paste0("^[0-9]{2}", subject, "I$"), id)]
+  if (length(match_id) > 0) {
+    data$raw_folder_name[i] <- paste0(match_id, "_EDF")
+    data$filename_id[i] <- match_id
+  }
+}
+data$raw_folder_name[data$subject == "O1"] <- "23O1I_EDF"
+data$filename_id[data$subject == "O1"] <- "23O1I"
+
 write.csv(data,file = "/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_releases/0.1.0.pre/isaps-dataset-0.1.0.pre.csv", row.names = FALSE, na='')
 
 # Harmonized data
@@ -22,3 +39,6 @@ harmonized_data<-data[,c("subject","age", "gender","timepoint")]%>%
                 )) %>% select(nsrrid,timepoint,nsrr_age,nsrr_sex)
 
 write.csv(harmonized_data, file = "/Volumes/BWH-SLEEPEPI-NSRR-STAGING/20191126-barger-data/nsrr-prep/_releases/0.1.0.pre/isaps-harmonized-dataset-0.1.0.pre.csv", row.names = FALSE, na='')
+
+
+
